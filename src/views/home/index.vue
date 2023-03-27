d0224b6951570deac9c8fc39ab33ec2bde2c4907
WRXinYue <3322543587@qq.com>
2023-03-28 04:49:35 +0800

    fix: 修复niao-bi页面并添加路由导航

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,19 +1,30 @@
 <template>
-  <h1 class="h-title">奈亚子练习主页</h1>
-  <el-link type="primary" @click="link(1)">bootstrap练习测试</el-link>
+  <div class="pl-40">
+    <h1 class="h-title">奈亚子练习主页</h1>
+    <div v-for="(link, index) in links" :key="index">
+      <el-link class="!text-xl" type="primary" @click="handleLinkClick(link)">
+        {{ link.label }}
+      </el-link>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
 import router from '@/router';
 
-const link = (id: number) => {
-  switch (id) {
-    case 1:
-      router.push('/bootstrap-home');
-      break;
-    default:
-      break;
-  }
+/** 定义跳转链接数组 */
+const links = [
+  { id: 1, label: 'bootstrap', path: '/bootstrap-home' },
+  { id: 2, label: 'niao-bi', path: '/niao-bi' },
+];
+// const links: { [key: number]: string } = {
+//   1: '/bootstrap-home',
+//   2: '/niao-bi',
+// };
+
+/** 执行链接跳转 */
+const handleLinkClick = (link: any): void => {
+  router.push(link.path);
 };
 </script>
 
--- src/views/project/niao-bi/index.vue
+++ src/views/project/niao-bi/index.vue
@@ -7,24 +7,10 @@
       <ul id="bq">
         <li id="fu" onclick="d">
           <a href="" style="font-weight: 600">播放BGM</a>
-          <audio src="./y/SAKURA.mp3" loop="loop" controls="controls" autoplay="autoplay" id="yy"></audio>
+          <audio id="yy" src="./src/views/project/niao-bi/y/SAKURA.mp3" loop controls autoplay></audio>
         </li>
         <li id="gywd">
-          <a href=""
-            >关于我们
-            <!-- <svg t="1650091143103" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2692" width="16" height="16">
-                    <path id="w" d="M895.441455 356.054626 511.529374 740.173576 128.558545 357.206626l38.798222-38.798222 344.067879 344.067879 345.219879-345.219879L895.441455 356.054626zM895.441455 356.054626" p-id="2693"></path></svg>  --></a
-          >
-          <!-- <div id="ejcd">
-                <div><p>漫剪文化</p></div>
-                <div><p>加入我们</p></div>
-                <div><p>联系我们</p></div>
-                <div><p>我们的团队</p></div>
-                <div id="heng">
-                    <button id="shang"></button>
-                    <svg t="1650093644103" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3123" width="32" height="32"><path d="M512 307.2c-6.4 0-19.2 0-25.6 6.4l-352 358.4c-12.8 12.8-12.8 32 0 44.8 12.8 12.8 32 12.8 44.8 0L512 377.6l332.8 332.8c12.8 12.8 32 12.8 44.8 0 12.8-12.8 12.8-32 0-44.8L531.2 313.6c0-6.4-12.8-6.4-19.2-6.4z" p-id="3124"></path></svg>
-                </div>
-            </div> -->
+          <a href="">关于我们slider-container</a>
         </li>
         <li><a href="">新闻动态</a></li>
         <li><a href="">创作服务平台</a></li>
@@ -34,27 +20,15 @@
       </ul>
     </div>
 
-    <div id="slider-container">
-      <video autoplay muted loop>
-        <source src="./t/385ca9d7-8941-4267-9c57-47aa80b4f25.mp4" type="video/mp4" />
+    <div id="y1">
+      <video v-for="(video, index) in videos" ref="slider" :key="index" autoplay muted loop>
+        <source :src="video" type="video/mp4" />
       </video>
-      <video autoplay muted loop>
-        <source src="./t/6d79432e-e78a-40f2-995b-733bab143f5d.mp4" type="video/mp4" />
-      </video>
-      <video autoplay muted loop>
-        <source src="./t/e17f4ac0-fafc-44b6-b5d6-b69bcd93a263.mp4" type="video/mp4" />
-      </video>
-      <video autoplay muted loop>
-        <source src="./t/1cee35a4-a599-4130-bea8-a11073a4b685.mp4" type="video/mp4" />
-      </video>
-      <ul id="indicator-container">
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
+      <ul id="yd">
+        <li v-for="(video, index) in videos" ref="indicators" :key="index"></li>
       </ul>
       <div class="hx"></div>
-      <ul id="caption-container">
+      <ul id="zi" ref="captions">
         <li style="display: block">
           那天，彗星划过天际的那一天，那就像，就像梦幻的景色一般，那真是无与伦比，美到极致的风景。——《你的名字》
         </li>
@@ -75,7 +49,6 @@
         <p>我们不是「二次元」</p>
         <p>我们只是二次元的搬运工</p>
       </div>
-      <!-- <div class="si"> -->
       <div id="si1">
         <img src="./t-1/1624232292627.jpg" alt="" />
         <img src="./t-1/1624232292387.jpg" alt="" />
@@ -91,44 +64,32 @@
         <img src="./t-2/1624232289707.jpg" alt="" />
         <img src="./t-2/1624232289677.jpg" alt="" />
       </div>
-      <!-- <div id="zuo"></div>
-            <div id='you'></div> -->
       <div id="si2">
-        <video autoplay loop muted src="./t/shu.mp4"></video>
-        <!-- <div id="si2-2"></div> -->
+        <video autoplay loop muted src="./video/shu.mp4"></video>
       </div>
       <div class="si3">
         <div class="si3-1">
-          <video autoplay loop muted src="./t/he.mp4"></video>
+          <video autoplay loop muted src="./video/he.mp4"></video>
         </div>
         <div class="si3-2">
-          <video autoplay loop muted src="./t/he1.mp4"></video>
+          <video autoplay loop muted src="./video/he1.mp4"></video>
         </div>
       </div>
     </div>
   </div>
   <div class="d2">
-    <div id="a">
-      <div class="log">
-        <img src="./y/QQ图片20220415161818.jpg" alt="" />
-      </div>
-      <div class="b" align="center">
-        <ul class="c">
-          <li>我的手里拿着刀，就无法抱着你。我把刀放下，就无法保护你</li>
-          <br />
-          <li>我不是天生的王者，但我骨子里流动着不让我低头的血液。</li>
-          <br />
-          <li>这个世界里没有什么规则，就算有，那也是由我创造的</li>
-          <br />
-          <li>正因为生命有限，所以才显得更重要，正因为生命有限、所以才更应该努力不懈。</li>
-        </ul>
-        <br /><br />
-        <div id="d">
-          <div><font size="5">联系方式：15760685730</font></div>
-          <br /><br />
-          <div><font size="5">制作人：剑侠客</font></div>
-          <br /><br />
-        </div>
+    <div class="log">
+      <img src="./y/QQ图片20220415161818.jpg" alt="" />
+    </div>
+    <div class="b" align="center">
+      <ul v-for="(item, index) in bottomTitles" ref="captions" :key="index" class="c">
+        <li>{{ item }}</li>
+      </ul>
+      <br />
+      <div id="d">
+        <div><font size="5">联系方式：15760685730</font></div>
+        <br />
+        <div><font size="5">制作人：剑侠客</font></div>
       </div>
     </div>
   </div>
@@ -144,51 +105,52 @@ $(function () {
 });
 
 const slider = ref();
-const indicator = ref();
-const caption = ref();
+const indicators = ref();
+const captions = ref();
 
-const videos = ref([]);
-const indicators = ref([]);
-const captions = ref([]);
+const videos = [
+  './src/views/project/niao-bi/video/385ca9d7-8941-4267-9c57-47aa80b4f25.mp4',
+  './src/views/project/niao-bi/video/6d79432e-e78a-40f2-995b-733bab143f5d.mp4',
+  './src/views/project/niao-bi/video/e17f4ac0-fafc-44b6-b5d6-b69bcd93a263.mp4',
+  './src/views/project/niao-bi/video/1cee35a4-a599-4130-bea8-a11073a4b685.mp4',
+];
+const bottomTitles = [
+  '我的手里拿着刀，就无法抱着你。我把刀放下，就无法保护你',
+  '我不是天生的王者，但我骨子里流动着不让我低头的血液',
+  '这个世界里没有什么规则，就算有，那也是由我创造的',
+  '正因为生命有限，所以才显得更重要，正因为生命有限、所以才更应该努力不懈',
+];
+
 // 定义当前元素的索引
-// let currentIndex = 0;
+let currentIndex = 0;
 
-onMounted(() => {
-  // 获取轮播图容器和元素列表
-  videos.value = slider.value.getElementsByTagName('video');
-  // 获取指示器容器和元素列表
-  indicators.value = indicator.value.getElementsByTagName('li');
-  // 获取说明文字容器和元素列表
-  captions.value = caption.value.getElementsByTagName('li');
-});
+// 更新轮播图状态的函数;
+function updateSlider() {
+  // 将当前索引加 1，并进行调整（如果达到上限则从头开始）
+  currentIndex += 1;
+  currentIndex = currentIndex === 4 ? 0 : currentIndex;
 
-// 更新轮播图状态的函数
-// function updateSlider() {
-//   // 将当前索引加 1，并进行调整（如果达到上限则从头开始）
-//   currentIndex++;
-//   currentIndex = currentIndex == 4 ? 0 : currentIndex;
+  // 隐藏所有视频元素，然后显示当前视频元素
+  for (let i = 0; i < videos.length; i += 1) {
+    slider.value[i].style.display = 'none';
+  }
+  slider.value[currentIndex].style.display = 'block';
 
-//   // 隐藏所有视频元素，然后显示当前视频元素
-//   for (let i = 0; i < videos.value.length; i++) {
-//     slider.value[i].style.display = 'none';
-//   }
-//   slider.value[currentIndex].style.display = 'block';
+  // 设置所有指示器元素的背景色为透明，然后设置当前指示器元素的背景色为白色
+  for (let i = 0; i < indicators.value.length; i += 1) {
+    indicators.value[i].style.backgroundColor = 'rgba(255, 250, 250, 0)';
+  }
+  indicators.value[currentIndex].style.backgroundColor = 'snow';
 
-//   // 设置所有指示器元素的背景色为透明，然后设置当前指示器元素的背景色为白色
-//   for (let i = 0; i < indicators.value.length; i++) {
-//     indicator.value[i].style.backgroundColor = 'rgba(255, 250, 250, 0)';
-//   }
-//   indicator.value[currentIndex].style.backgroundColor = 'snow';
-
-//   // 隐藏所有说明文字元素，然后显示当前说明文字元素
-//   for (let i = 0; i < captions.value.length; i++) {
-//     caption.value[i].style.display = 'none';
-//   }
-//   caption.value[currentIndex].style.display = 'block';
-// }
+  // 隐藏所有说明文字元素，然后显示当前说明文字元素
+  for (let i = 0; i < captions.value.length; i += 1) {
+    captions.value[i].style.display = 'none';
+  }
+  captions.value[currentIndex].style.display = 'block';
+}
 
 // 每隔 3 秒钟调用一次更新函数
-// const sliderInterval = setInterval(updateSlider, 3000);
+const sliderInterval = setInterval(updateSlider, 3000);
 </script>
 
 <style lang="scss" scoped>
@@ -202,12 +164,12 @@ onMounted(() => {
     perspective: 800px;
 } */
 .d {
-  width: 1536px;
+  width: 100%;
   height: 745px;
   /* background-color: brown; */
 }
 .h {
-  width: 1536px;
+  width: 100%;
   height: 102px;
   background-color: rgb(214, 213, 213);
   position: absolute;
@@ -216,7 +178,7 @@ onMounted(() => {
   position: absolute;
   width: 200px;
   height: 100px;
-  background-image: url(../t/bear.png);
+  background-image: url(./video/bear.png);
   animation: al 1s steps(8) infinite, ll 3s forwards;
 }
 @keyframes al {
@@ -236,8 +198,8 @@ onMounted(() => {
   }
 }
 #bq {
-  width: 921px;
-  height: 122px;
+  width: 69%;
+  height: 100%;
   /* background-color: gold; */
   float: right;
   padding: 0;
@@ -255,7 +217,7 @@ onMounted(() => {
   top: 10px;
 }
 #ejcd {
-  width: 1536px;
+  width: 100%;
   height: 420px;
   background-color: rgb(255, 255, 255);
   position: absolute;
@@ -321,7 +283,7 @@ onMounted(() => {
 }
 
 #y1 {
-  width: 100vw;
+  width: 100%;
   height: 100vh;
   /* background-color: bisque; */
   margin-top: 100px;
@@ -377,22 +339,20 @@ onMounted(() => {
   display: none;
 }
 .d1 {
-  width: 1536px;
+  width: 100%;
   height: 745px;
   background-color: rgb(255, 255, 255);
   /* display: none; */
 }
 .he2 {
-  height: 422px;
-  width: 1274px;
-  /* background-color: darkred; */
   position: absolute;
-  margin-top: 3%;
-  margin-left: 8%;
+  margin-top: 4px;
+  margin-left: 16.3%;
 }
 .hang {
   width: 1274px;
   height: 128px;
+  margin-bottom: 12px;
   /* background-color: darkseagreen; */
 }
 .hang p {
@@ -452,11 +412,10 @@ onMounted(() => {
   border-radius: 10px;
 }
 #si2 video {
-  width: 40vw;
-  height: 66vh;
+  width: auto;
+  height: auto;
   position: absolute;
   overflow: hidden;
-  margin-left: -169px;
   /* border-radius: 20px; */
   transition: all 0.2s;
 }
@@ -493,9 +452,8 @@ onMounted(() => {
   box-shadow: 0 0 15px rgb(0, 0, 0, 0.3);
 }
 .si3-1 video {
-  width: 35vw;
-  height: 40vh;
-  margin-left: -43px;
+  width: auto;
+  height: auto;
 }
 .si3-2 {
   width: 451px;
@@ -510,29 +468,26 @@ onMounted(() => {
   box-shadow: 0 0 15px rgb(0, 0, 0, 0.3);
 }
 .si3-2 video {
-  width: 35vw;
-  height: 40vh;
-  margin-left: -43px;
+  width: auto;
+  height: auto;
 }
 .d2 {
-  width: 1536px;
+  width: 100%;
   height: 745px;
   background-color: rgb(0, 0, 0);
 }
 .d2 {
-  width: 1536px;
+  width: 100%;
   height: 745px;
   background-color: rgb(0, 0, 0);
   /* margin-top: 10px; */
   float: left;
 }
 .log {
-  width: 200px;
+  width: 100%;
   height: 100px;
   /* background-color: bisque; */
   float: left;
-  margin-left: 120px;
-  margin-left: 0px;
 }
 @keyframes dh {
   0% {
@@ -547,20 +502,21 @@ onMounted(() => {
   height: 130px;
   border-radius: 50%;
   margin-top: 170px;
-  margin-left: 731px;
+  margin-left: 46.5%;
   animation: dh 7s infinite linear;
 }
 #a {
   background-color: rgb(0, 0, 0);
-  height: 588px;
-  width: 1536px;
+  // height: 588px;
+  width: 100%;
   /* margin-top: -68px; */
 }
 .b {
+  height: auto;
+  width: 100%;
   color: white;
-  margin-top: 380px;
+  margin-top: 280px;
   float: left;
-  margin-left: 300px;
 }
 .c {
   list-style-type: none;
